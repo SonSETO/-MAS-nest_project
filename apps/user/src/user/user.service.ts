69a995d9e2847d561c8c462c9f06f51894bc69c0
yesthis9090@gmail.com <yesthis9090@gmail.com>
2024-12-12 00:09:44 +0900

    MAS user,product

---
+++ apps/user/src/user/user.service.ts
@@ -0,0 +1,43 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password } = createUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      throw new BadRequestException('이미 가입한 이메일 입니다!');
+    }
+
+    // 나중에 환경변수로
+    const hash = await bcrypt.hash(password, 10);
+
+    await this.userRepository.save({
+      ...createUserDto,
+      email,
+      password: hash,
+    });
+
+    return this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
+  }
+}
